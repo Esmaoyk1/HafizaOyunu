1306715d4c0eec63be61fa6c641ca1d93278d7a0
esmanur oyanık <esmaoyanik55@gmail.com>
2024-05-21 19:52:36 +0300

    Add project files.

---
+++ Oyun/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Oyun
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.GradientActiveCaption;
+            button1.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button1.Location = new Point(130, 47);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 91);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += Button_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.GradientActiveCaption;
+            button2.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button2.Location = new Point(248, 47);
+            button2.Name = "button2";
+            button2.Size = new Size(134, 91);
+            button2.TabIndex = 0;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += Button_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.GradientActiveCaption;
+            button3.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button3.Location = new Point(366, 47);
+            button3.Name = "button3";
+            button3.Size = new Size(134, 91);
+            button3.TabIndex = 0;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += Button_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.GradientActiveCaption;
+            button4.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button4.Location = new Point(484, 47);
+            button4.Name = "button4";
+            button4.Size = new Size(134, 91);
+            button4.TabIndex = 0;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += Button_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.GradientActiveCaption;
+            button6.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button6.Location = new Point(130, 132);
+            button6.Name = "button6";
+            button6.Size = new Size(134, 91);
+            button6.TabIndex = 0;
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += Button_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.GradientActiveCaption;
+            button7.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button7.Location = new Point(248, 132);
+            button7.Name = "button7";
+            button7.Size = new Size(134, 91);
+            button7.TabIndex = 0;
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += Button_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.GradientActiveCaption;
+            button8.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button8.Location = new Point(366, 132);
+            button8.Name = "button8";
+            button8.Size = new Size(134, 91);
+            button8.TabIndex = 0;
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += Button_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.GradientActiveCaption;
+            button9.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button9.Location = new Point(484, 132);
+            button9.Name = "button9";
+            button9.Size = new Size(134, 91);
+            button9.TabIndex = 0;
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += Button_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = SystemColors.GradientActiveCaption;
+            button11.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button11.Location = new Point(130, 217);
+            button11.Name = "button11";
+            button11.Size = new Size(134, 91);
+            button11.TabIndex = 0;
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += Button_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = SystemColors.GradientActiveCaption;
+            button12.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button12.Location = new Point(248, 217);
+            button12.Name = "button12";
+            button12.Size = new Size(134, 91);
+            button12.TabIndex = 0;
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += Button_Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = SystemColors.GradientActiveCaption;
+            button13.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button13.Location = new Point(366, 217);
+            button13.Name = "button13";
+            button13.Size = new Size(134, 91);
+            button13.TabIndex = 0;
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += Button_Click;
+            // 
+            // button14
+            // 
+            button14.BackColor = SystemColors.GradientActiveCaption;
+            button14.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button14.Location = new Point(484, 217);
+            button14.Name = "button14";
+            button14.Size = new Size(134, 91);
+            button14.TabIndex = 0;
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += Button_Click;
+            // 
+            // button16
+            // 
+            button16.BackColor = SystemColors.GradientActiveCaption;
+            button16.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button16.Location = new Point(130, 302);
+            button16.Name = "button16";
+            button16.Size = new Size(134, 91);
+            button16.TabIndex = 0;
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += Button_Click;
+            // 
+            // button17
+            // 
+            button17.BackColor = SystemColors.GradientActiveCaption;
+            button17.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button17.Location = new Point(248, 302);
+            button17.Name = "button17";
+            button17.Size = new Size(134, 91);
+            button17.TabIndex = 0;
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += Button_Click;
+            // 
+            // button18
+            // 
+            button18.BackColor = SystemColors.GradientActiveCaption;
+            button18.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button18.Location = new Point(366, 302);
+            button18.Name = "button18";
+            button18.Size = new Size(134, 91);
+            button18.TabIndex = 0;
+            button18.UseVisualStyleBackColor = false;
+            button18.Click += Button_Click;
+            // 
+            // button19
+            // 
+            button19.BackColor = SystemColors.GradientActiveCaption;
+            button19.Font = new Font("Webdings", 36F, FontStyle.Bold);
+            button19.Location = new Point(484, 302);
+            button19.Name = "button19";
+            button19.Size = new Size(134, 91);
+            button19.TabIndex = 0;
+            button19.UseVisualStyleBackColor = false;
+            button19.Click += Button_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button19);
+            Controls.Add(button14);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button8);
+            Controls.Add(button16);
+            Controls.Add(button7);
+            Controls.Add(button11);
+            Controls.Add(button3);
+            Controls.Add(button6);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            ForeColor = SystemColors.GradientActiveCaption;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+    }
+}
